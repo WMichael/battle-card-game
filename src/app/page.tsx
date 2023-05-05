c3c090ad6ff5394be59dfb33cd37580e97dd8a83
Michael Weller <vve11ermichae1@gmail.com>
2023-05-05 23:06:51 +0100

    Add PlayingCardsMat and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
+import PlayingCardsMat from '@/components/PlayCardsMat/PlayingCardsMat';
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Battle</h1>
-      <h2>The card game</h2>
-    </div>
+    <PlayingCardsMat></PlayingCardsMat>
   )
 }
---
+++ src/components/PlayCardsMat/PlayingCardsMat.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PlayingCardsMat.css';
+
+export default function PlayingCardsMat() {
+  return (
+    <div className='mat'>
+        
+    </div>
+  )
+}
